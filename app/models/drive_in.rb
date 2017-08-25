25c550cbeeda891eb986bf0cfcb3915d74b42992
macro6461 <mattcroak718@gmail.com>
2017-08-25 10:58:52 -0400

    DONE

--- app/models/drive_in.rb
+++ app/models/drive_in.rb
@@ -1,7 +1,43 @@
+require 'pry'
 class DriveIn
 
-  def initialize
+  attr_accessor :name
+
+  def initialize(name)
     @name = "Happy's Sunset Drive-In"
   end
-  
+
+  def cars
+    self.screens.collect do |screen|
+      screen.cars
+    end
+  end
+
+  def screens
+    MovieScreen.all.select do |movie_screen|
+      movie_screen.drive_in == self
+    end
+  end
+
+  def full_house
+    # returns true if all movie screens are at capacity
+    capacities = self.screens.collect do |movie_screen|
+      movie_screen.at_capacity
+    end
+    capacities.all? {|x| x == true}
+  end
+
+  def whats_playing
+    playing_hash = {}
+    self.screens.each do |movie_screen|
+      playing_hash[movie_screen.movie] = {:available_seats => (movie_screen.capacity - movie_screen.cars.size)}
+    end
+    playing_hash
+  end
+
+  # #{
+  #   "Spider Man" => {:available_seats => 12},
+  #   "Cinderalla" => {:available_seats => 0}
+  # }
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,5 +3,16 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
+sunset = DriveIn.new("name")
+
+
+moviescreen1 = MovieScreen.new("Departed", 120, sunset)
+moviescreen2 = MovieScreen.new("Little Mermaid", 110, sunset)
+
+car1 = Car.new("Car1", moviescreen1)
+car2 = Car.new("Car2", moviescreen1)
+car3 = Car.new("Car3", moviescreen1)
+
+
 
 Pry.start
